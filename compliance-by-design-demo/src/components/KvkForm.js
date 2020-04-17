cc0f03eb4576875f6910092d3fb4b3da7f2f1239
Pim Otte <otte.pim@gmail.com>
2020-04-17 10:36:50 +0200

    Remove pre-filled kvk number

--- compliance-by-design-demo/src/components/KvkForm.js
+++ compliance-by-design-demo/src/components/KvkForm.js
@@ -10,7 +10,7 @@ class KvkForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      kvkNumber: "69599084",
+      kvkNumber: "",
       kvkNumberEntered: false,
       derivedFacts: null,
     };
